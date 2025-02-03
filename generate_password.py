2e2a0faa289ac5ae81b118634e337f2ab1f7bf87
ppashchenkov <ppashchenkov@gmail.com>
2025-02-03 19:04:35 +0300

    add generate_password.py

--- generate_password.py
+++ generate_password.py
@@ -1,16 +1,17 @@
 import random
+import re
 
 
 def generate_password(password_lenth):
 
-    small_letters = 'abcdefghijklmnopqrstuvwxyz'
-    big_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    small_letters = 'abcdefghijkmnpqrstuvwxyz'
+    big_letters = 'ABCDEFGHJKLMNPQRSTUVWXYZ'
     numbers = '0123456789'
     symbols = '!@#$%^&*()_-+~`=:;?,.{}|[]'
     chars = big_letters + small_letters + numbers + symbols
 
-    if password_lenth < 4:
-        password_size = 4
+    if password_lenth < 5:
+        password_size = 5
     elif password_lenth > 64:
         password_size = 64
     else:
@@ -23,13 +24,33 @@ def generate_password(password_lenth):
 
         return result
 
-    # def all_cases(string):
-    #     upper = / [A - Z] /.test(string)
+    num_pattern = r'[0-9]'
+    big_char_pattern = r'[A-Z]'
+    small_char_pattern = r'[a-z]'
+    symbol_pattern = r'[/!/@/#/$/%/^/&/*/(/)/_/-/+/~/`/=/:/;/?/,/./{/}/|/[/]]'
 
+    def is_exist(pattern, text):
+        return re.search(pattern, text) is not None
 
-    return generate_random_string(password_size, chars)
+    def is_valid(str):
+        return is_exist(num_pattern, str) and \
+            is_exist(big_char_pattern, str) and \
+            is_exist(small_char_pattern, str) and \
+            is_exist(symbol_pattern, str)
 
-passwd = generate_password(14)
+    iterations = 1
+    password = None
+    while True:
+        password = generate_random_string(password_size, chars)
+        if is_valid(password):
+            break
+        iterations += 1
+
+    print("_________________________________")
+    print(f"Количество иттераций = {iterations}")
+    return password
+
+passwd = generate_password(4)
 
 print("_________________________________")
 print(f"Пароль: {passwd}")
