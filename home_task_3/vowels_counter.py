962d622e0b6c46aef868cc9669e87486a425b05d
ppashchenkov <ppashchenkov@gmail.com>
2025-02-25 20:21:38 +0300

    refactoring calculator_test.py

--- home_task_3/vowels_counter.py
+++ home_task_3/vowels_counter.py
@@ -4,7 +4,7 @@ import re
 message = input("Введите любое слово или фразу :")
 vowels_pattern = r'[аАеЕёЁиИоОуУыЫэЭюЮяЯaAeEiIoOuUyY]'
 vowels_chars = re.findall(vowels_pattern, message)
-#print(vowels_chars)
+print(vowels_chars)
 if vowels_chars is not None:
     print(f"Количество гласных букв = {len(vowels_chars)}")
 else:
--- pytest/calculator_test.py
+++ pytest/calculator_test.py
@@ -5,40 +5,40 @@ import pytest
 calc = Calculator()
 
 @pytest.mark.parametrize(
-    "a, b, expected",
+    "a_, b_, expected_",
     [
         (5, 7, 12),
         (0, -7, -7),
         (-10, 7, -3),
         (99, -9, 90)
     ])
-def test_sum(a, b, expected):
-    assert calc.sum(a, b) == expected
+def test_sum(a_, b_, expected_):
+    assert calc.sum(a_, b_) == expected_
 
 @pytest.mark.parametrize(
-    "a, b, expected",
+    "a_, b_, expected_",
     [
         (5, 7, -2),
         (0, -7, 7),
         (-10, 7, -17),
         (99, -9, 108)
     ])
-def test_sub(a, b, expected):
-    assert calc.sub(a, b) == expected
+def test_sub(a_, b_, expected_):
+    assert calc.sub(a_, b_) == expected_
 
 @pytest.mark.parametrize(
-    "a, b, expected",
+    "a_, b_, expected_",
     [
         (5, 7, 35),
         (0, -7, 0),
         (-10, 7, -70),
         (99, -9, -891)
     ])
-def test_mult(a, b, expected):
-    assert calc.mult(a, b) == expected
+def test_mult(a_, b_, expected_):
+    assert calc.mult(a_, b_) == expected_
 
 @pytest.mark.parametrize(
-    "a, b, expected",
+    "a_, b_, expected_",
     [
         (5, 2, 25),
         (5, 3, 125),
@@ -46,34 +46,34 @@ def test_mult(a, b, expected):
         (2, -1, 0.5),
         (4, -2, 0.0625)
     ])
-def test_power(a, b, expected):
-    assert calc.power(a, b) == expected
+def test_power(a_, b_, expected_):
+    assert calc.power(a_, b_) == expected_
 
 @pytest.mark.parametrize(
-    "a, b, expected",
+    "a_, b_, expected_",
     [
         (14, 7, 2),
         (0, -7, 0),
         (-7, 7, -1),
         (99, -9, -11)
     ])
-def test_div(a, b, expected):
-    assert calc.div(a, b) == expected
+def test_div(a_, b_, expected_):
+    assert calc.div(a_, b_) == expected_
 
 @pytest.mark.xfail(raises=ArithmeticError)
 def test_div_on_zero():
     assert calc.div(5, 0) == 5
 
 @pytest.mark.parametrize(
-    "nums, expected",
+    "nums_, expected_",
     [
         ([], 0),
         ([2,2,3,5], 3),
         ([0, 8], 4),
         ([5], 5)
     ])
-def test_avr(nums, expected):
-    assert calc.avr(nums) == expected
+def test_avr(nums_, expected_):
+    assert calc.avr(nums_) == expected_
 
 @pytest.mark.xfail(raises=Exception)
 def test_avr_nums_strings():
