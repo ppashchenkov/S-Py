0d811a6ac99abb17b305ae07d6c34d423b4bea9e
ppashchenkov <email@example.com>
2025-03-31 15:30:58 +0300

    alert_cats.py Done!

--- uitap_learning/alert_cats.py
+++ uitap_learning/alert_cats.py
@@ -1,10 +1,12 @@
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
+chrome_service = Service()
 driver = webdriver.Chrome()
 waiter5 = WebDriverWait(driver, 5)
 
@@ -20,26 +22,21 @@ print("Click Alert")
 confirm_id = 'confirmButton'
 driver.find_element(By.ID, confirm_id).click()
 waiter5.until(EC.alert_is_present())
-alert = driver.switch_to.alert
 alert.accept()
-
+waiter5.until(EC.alert_is_present())
+alert.accept()
 print("Click Confirm")
 
 prompt_id = 'promptButton'
 driver.find_element(By.ID, prompt_id).click()
 waiter5.until(EC.alert_is_present())
-alert = driver.switch_to.alert
 alert.send_keys("ddd")
-# print(alert.text)
-# sleep(2)
 alert.accept()
 
 waiter5.until(EC.alert_is_present())
-alert = driver.switch_to.alert
-text = alert.text
-
+text = driver.switch_to.alert.text
 print("Text = ", text)
+sleep(2)
 alert.accept()
-
 
 driver.quit()
